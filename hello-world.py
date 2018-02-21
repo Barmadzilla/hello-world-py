3bc989fbc97243203db9a65a09626f9130316ac8
Barmadzilla <v@cararts.ru>
2018-02-21 22:29:03 +0300

    for heroku

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,11 @@
+from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+app.run(environ.get('PORT'))
+
 msg = 'Python say "Hello World"'
 c = len(msg) + 4
 print("+" * c)
 print("+", msg, "+")
 print("+" * c)
-
